06ce1e9758db19d742e61aeb920593b52139d382
DarkHack-ink <shidhartsharma183@gmail.com>
2025-11-29 16:39:04 +0530

    Add files via upload
    practicing whatsapp message autaumation trhough pywhatkit library in python

---
+++ pywhat_lib.py
@@ -0,0 +1,6 @@
+""" task 1. 5 members number in a textfile using that textfile pick numbers from it and send them to it"""
+
+import pywhatkit
+pywhatkit.sendwhatmsg_instantly("+917023047116", "hii heloo")
+
+
