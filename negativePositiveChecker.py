8f36a6e1b63145ccb0642185fbdc0103ed5fd460
DarkHack-ink <shidhartsharma183@gmail.com>
2025-11-29 16:48:02 +0530

    Add files via upload
    practicing python

---
+++ negativePositiveChecker.py
@@ -0,0 +1,7 @@
+z = int(input("Enter the a number to check it is positive , negative or zero :"))
+if z > 0:
+  print("The number is positive")
+elif z < 0:
+  print("The number is negative")
+else :
+  print("The number is zero")
